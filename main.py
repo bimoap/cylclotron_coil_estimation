f6eb5351e5bb14ae91655fe499e7ee2b7c600caa
Bimo Adhi Prastya <bimoap@users.noreply.github.com>
2026-03-15 10:04:53 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import numpy as np
-import matplotlib.subplots as plt_sub # Wait, user specifically said "Always Remove import matplotlib.subplots as plt_sub"
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import pint
